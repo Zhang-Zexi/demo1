f57291cc938bd0111b5b7a1a51981c2b6d21d8a5
zhangzx <zhangzx@allinfinance.com>
2019-08-01 17:09:20 +0800

    反射-获取成员方法

--- src/com/zzx/reflex2/Fields.java
+++ src/com/zzx/reflex2/Fields.java
@@ -45,3 +45,21 @@ public class Fields {
         System.out.println("验证电话：" + stu);
     }
 }
+
+/*
+ * 获取成员变量并调用：
+ *
+ * 1.批量的
+ *      1).Field[] getFields():获取所有的”公有字段”
+ *      2).Field[] getDeclaredFields():获取所有字段，包括：私有、受保护、默认、公有；
+ * 2.获取单个的：
+ *      1).public Field getField(String fieldName):获取某个”公有的”字段；
+ *      2).public Field getDeclaredField(String fieldName):获取某个字段(可以是私有的)
+ *
+ *   设置字段的值：
+ *      Field –> public void set(Object obj,Object value):
+ *                  参数说明：
+ *                  1.obj:要设置的字段所在的对象；
+ *                  2.value:要为字段设置的值；
+ *
+ */
