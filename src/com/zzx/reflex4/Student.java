6b53484ac03596fe3bc672e48d0d3b0642f51188
zhangzx <zhangzx@allinfinance.com>
2019-08-01 17:36:03 +0800

    反射-获取main方法

---
+++ src/com/zzx/reflex4/Student.java
@@ -0,0 +1,15 @@
+package com.zzx.reflex4;
+
+/**
+ * @ClassName Student
+ * @Description
+ * @Author zhangzx
+ * @Date 2019/8/1 17:11
+ * Version 1.0
+ **/
+public class Student {
+
+    public static void main(String[] args) {
+        System.out.println("main方法执行了。。。");
+    }
+}
