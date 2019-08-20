49fdae072fa48cb550e630304e9965fa603ebeb2
zhangzx <zhangzx@allinfinance.com>
2019-08-20 14:40:53 +0800

    String.reverse()方法

--- src/com/zzx/arrays/Demo02.java
+++ src/com/zzx/arrays/Demo02.java
@@ -11,9 +11,21 @@ import java.io.UnsupportedEncodingException;
  **/
 public class Demo02 {
 
+    public static String reverse(String originStr) {
+        if (originStr == null || originStr.length() <= 1) {
+            return originStr;
+        }
+//        System.out.println(originStr.substring(1));
+//        System.out.println(originStr.charAt(0));
+        return reverse(originStr.substring(1)) + originStr.charAt(0);
+    }
+
     public static void main(String[] args) throws Exception {
-        String s1 = "你好";
-        String s2 = new String(s1.getBytes("GB2312"), "ISO-8859-1");
-        System.out.println(s2);
+//        String s1 = "你好";
+//        String s2 = new String(s1.getBytes("GB2312"), "ISO-8859-1");
+//        System.out.println(s2);
+
+        String s3 = "ABCDEFG";
+        System.out.println(reverse(s3));
     }
 }
